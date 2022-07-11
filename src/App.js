c08be81c2754e48047df05b041a81c2e6d4744da
Sahil Shende <ssahilshende@gmail.com>
2022-07-11 21:15:02 +0530

    add footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import React from "react";
 import Home from "./Home";
 import Header from "./Header";
+import Footer from "./Footer";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <div className="app">
       <Header />
       <Home />
+      <Footer />
       {/* Home */}
       {/* Header */}
       {/* Banner */}
